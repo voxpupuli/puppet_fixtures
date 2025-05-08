voxpupuli/puppet_fixtures

commit 4557f8fe366882d8c11e6cf4d3a0cca62088d5c9

Refactor to a separate class

lib/puppet_fixtures/tasks.rb
@@ -5,7 +5,7 @@ require 'rake'
 namespace :fixtures do
   desc 'Create the fixtures directory'
   task :prep do
-    PuppetFixtures.new.download
+    PuppetFixtures.new.download(max_thread_limit: ENV.fetch('MAX_FIXTURE_THREAD_COUNT', 10).to_i)
   end
 
   desc 'Clean up the fixtures directory'

.gitignore
@@ -1,1 +1,4 @@
+*.gem
+/.fixtures.yml
 Gemfile.lock
+spec/fixtures/modules

Gemfile
@@ -2,6 +2,9 @@ source 'https://rubygems.org'
 
 gemspec
 
+# On Windows there's bug PUP-4884 in < 7.9
+gem 'puppet', '>= 7.9'
+
 group :test do
   gem 'rspec', require: false
 end

bin/puppet-fixtures
@@ -1,0 +1,59 @@
+#!/usr/bin/env ruby
+
+require 'pathname'
+require 'optparse'
+
+require 'puppet_fixtures'
+
+COMMANDS = ['clean', 'install', 'show']
+
+command = ARGV[0]
+unless COMMANDS.include?(command)
+  $stderr.puts "Usage: #{$0} #{COMMANDS.join('|')}"
+  exit 1
+end
+
+fixtures = PuppetFixtures::Fixtures.new
+
+case command
+when 'clean'
+  fixtures.clean
+when 'install'
+  fixtures.download
+when 'show'
+  if (path = fixtures.fixture_path)
+    puts "Parsing #{fixtures.fixture_path}"
+
+    if fixtures.symlinks.any?
+      puts
+      puts "Symlinks"
+      fixtures.symlinks.each do |_target, symlink|
+        puts "  #{symlink}"
+      end
+    end
+
+    if fixtures.forge_modules.any?
+      puts
+      puts "Forge modules"
+      fixtures.forge_modules.each do |mod, opts|
+        dir = Pathname.new(opts[:target]).relative_path_from(fixtures.module_target_dir)
+        description = mod
+        description += " #{opts[:ref]}"
+        puts "  #{dir} => #{description}"
+      end
+    end
+
+    if fixtures.repositories.any?
+      puts
+      puts "Repositories"
+      fixtures.repositories.each do |repository, opts|
+        dir = Pathname.new(opts[:target]).relative_path_from(fixtures.module_target_dir)
+        description = ["#{opts[:scm]}+#{repository}", opts[:ref]]
+        description << "branch #{opts[:branch]}" if opts[:branch]
+        puts "  #{dir} => #{description.compact.join(' ')}"
+      end
+    end
+  else
+    $stderr.puts "No fixture file found"
+  end
+end

puppet_fixtures.gemspec
@@ -1,15 +1,16 @@
 Gem::Specification.new do |s|
-  s.name        = 'puppet_fixtures'
-  s.version     = '0.1.0'
-  s.licenses    = ['Apache-2.0']
-  s.summary     = 'Set up fixtures for Puppet testing'
-  s.description = <<~DESC
+  s.name                 = 'puppet_fixtures'
+  s.version              = '0.1.0'
+  s.licenses             = ['Apache-2.0']
+  s.summary              = 'Set up fixtures for Puppet testing'
+  s.description          = <<~DESC
     Originally part of puppetlabs_spec_helper, but with a significant
     refactoring to make it available standalone.
   DESC
-  s.authors     = ['Ewoud Kohl van Wijngaarden']
-  s.email       = 'rubycoder@example.com'
-  s.files       = ['lib/example.rb', 'lib/puppet_fixtures/tasks.rb']
-  s.homepage    = 'https://rubygems.org/gems/example'
-  s.metadata    = { 'source_code_uri' => 'https://github.com/example/example' }
+  s.authors               = ['Ewoud Kohl van Wijngaarden']
+  s.files                 = ['lib/puppet_fixtures.rb', 'lib/puppet_fixtures/tasks.rb']
+  s.executables           << 'puppet-fixtures'
+  s.homepage              = 'https://github.com/voxpupuli/puppet_fixtures'
+  s.metadata              = { 'source_code_uri' => 'https://github.com/voxpupuli/puppet_fixtures' }
+  s.required_ruby_version = '>= 2.7', '< 4'
 end

commit 631fe331569446cf2085df9736018557ea420b9e

Call the correct constructor in rake tasks
This adds basic acceptance tests by running the rake tasks. By default
they should be noop since there is no fixture file.

Fixes: 4557f8fe366882d8c11e6cf4d3a0cca62088d5c9 ("Refactor to a separate class")

.github/workflows/test.yml
@@ -39,12 +39,11 @@ jobs:
           bundler-cache: true
       - name: Verify gem builds
         run: gem build --strict --verbose *.gemspec
-      - name: check that required tasks are available
-        run: |
-          bundle exec rake --rakefile lib/puppet_fixtures/tasks.rb -T 'fixtures:clean' | grep --quiet .
-          bundle exec rake --rakefile lib/puppet_fixtures/tasks.rb -T 'fixtures:prep' | grep --quiet .
       - name: Run rspec tests
         run: bundle exec rake spec
+      - name: Run acceptance tests
+        run: |
+          bundle exec rake --rakefile lib/puppet_fixtures/tasks.rb fixtures:prep fixtures:clean
 
   tests:
     needs:

lib/puppet_fixtures/tasks.rb
@@ -5,11 +5,11 @@ require 'rake'
 namespace :fixtures do
   desc 'Create the fixtures directory'
   task :prep do
-    PuppetFixtures.new.download(max_thread_limit: ENV.fetch('MAX_FIXTURE_THREAD_COUNT', 10).to_i)
+    PuppetFixtures::Fixtures.new(max_thread_limit: ENV.fetch('MAX_FIXTURE_THREAD_COUNT', 10).to_i).download
   end
 
   desc 'Clean up the fixtures directory'
   task :clean do
-    PuppetFixtures.new.clean
+    PuppetFixtures::Fixtures.new.clean
   end
 end
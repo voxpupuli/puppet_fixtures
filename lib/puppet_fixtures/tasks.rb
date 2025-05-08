631fe331569446cf2085df9736018557ea420b9e
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2025-05-08 16:45:54 +0200

    Call the correct constructor in rake tasks
    This adds basic acceptance tests by running the rake tasks. By default
    they should be noop since there is no fixture file.
    
    Fixes: 4557f8fe366882d8c11e6cf4d3a0cca62088d5c9 ("Refactor to a separate class")

--- lib/puppet_fixtures/tasks.rb
+++ lib/puppet_fixtures/tasks.rb
@@ -5,11 +5,11 @@ require 'rake'
 namespace :fixtures do
   desc 'Create the fixtures directory'
   task :prep do
-    PuppetFixtures.new.download(max_thread_limit: ENV.fetch('MAX_FIXTURE_THREAD_COUNT', 10).to_i)
+    PuppetFixtures::Fixtures.new(max_thread_limit: ENV.fetch('MAX_FIXTURE_THREAD_COUNT', 10).to_i).download
   end
 
   desc 'Clean up the fixtures directory'
   task :clean do
-    PuppetFixtures.new.clean
+    PuppetFixtures::Fixtures.new.clean
   end
 end
